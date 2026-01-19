f0a3213ce2486dc2ed5325819ad6ff1fd51e2f8d
obadx <abdullahamlyossef@gmail.com>
2026-01-19 13:23:00 +0200

    add: skelton

---
+++ src/quran_transcript/phonetics/error_explainer.py
@@ -0,0 +1,317 @@
+from abc import abstractmethod, ABC
+from typing import Literal, Optional
+from dataclasses import dataclass, replace
+
+
+from .sifa import chunck_phonemes
+from .. import alphabet as alph
+import Levenshtein as lv
+
+
+from .tajweed_rulses import TajweedRule, NormalMaddRule
+
+
+@dataclass
+class MappingPos:
+    pos: tuple[int, int]
+    tajweed_rules: list[TajweedRule] | None = None
+
+
+@dataclass
+class ReciterError:
+    uthmani_pos: tuple[int, int]  # start, end
+    ph_pos: tuple[int, int]  # start, end
+    error_type: Literal["tajweed", "normal", "tashkeel"]
+    speech_error_type: Literal["insert", "delete", "replace"]
+    expected_ph: str
+    preditected_ph: str
+    expected_len: Optional[int] | None = None
+    predicted_len: Optional[int] | None = None
+    tajweed_rules: Optional[list[TajweedRule]] | None = None
+    predicted_tajweed_rules: Optional[list[TajweedRule]] | None = None
+
+
+@dataclass
+class PhonemesGroupsAligment:
+    op_type: Literal["insert", "replace", "delete", "equal"]
+    ref_idx: int
+    pred_idx: int
+
+
+# TODO: Testing
+def align_phonemes_groups(
+    ref_ph_groups: list[str], pred_ph_groups: list[str]
+) -> list[PhonemesGroupsAligment]:
+    ref_text = [t[0] for t in ref_ph_groups]
+    pred_text = [t[0] for t in pred_ph_groups]
+    out_aligments = []
+
+    for out in lv.opcodes(ref_text, pred_text):
+        # disassemble operations so every item contains a single change for a single index
+        if out[0] in {"equal", "replace"}:
+            for ref_start, pred_start in zip(
+                range(out[1], out[2]), range(out[3], out[4])
+            ):
+                out_aligments.append(
+                    PhonemesGroupsAligment(
+                        op_type=out[0],
+                        ref_idx=ref_start,
+                        pred_idx=pred_start,
+                    )
+                )
+        elif out[0] == "insert":
+            for pred_start in range(out[3], out[4]):
+                out_aligments.append(
+                    PhonemesGroupsAligment(
+                        op_type="insert",
+                        ref_idx=out[1],
+                        pred_idx=pred_start,
+                    )
+                )
+        elif out[0] == "delete":
+            for ref_start in range(out[1], out[2]):
+                out_aligments.append(
+                    PhonemesGroupsAligment(
+                        op_type="delete",
+                        ref_idx=ref_start,
+                        pred_idx=out[3],
+                    )
+                )
+
+    return out_aligments
+
+
+def extract_ref_phonetic_to_uthmani(
+    mappings: list[MappingPos | None],
+) -> dict[int, int]:
+    ref_ph_to_uthmani = {}
+    for idx, map_pos in enumerate(mappings):
+        if map_pos is not None:
+            for ph_idx in range(*map_pos.pos):
+                if ph_idx in ref_ph_to_uthmani:
+                    raise ValueError(
+                        f"Same phonetic scripts has multiple uthmani script. Phonetic posision: `{ph_idx}`, Uthmani Poses: `{ref_ph_to_uthmani[ph_idx]}, {idx}`"
+                    )
+                else:
+                    ref_ph_to_uthmani[ph_idx] = idx
+    return ref_ph_to_uthmani
+
+
+def get_ref_phonetic_groups_tajweed_rules(
+    ref_ph_groups: list[str],
+    mappings: list[MappingPos | None],
+    ref_ph_to_uthmani: dict[int, int],
+) -> list[None | list[TajweedRule]]:
+    ref_tajweed_rules = [None] * len(ref_ph_groups)
+    start = 0
+    end = 0
+    # Computing Tajweed rules
+    # TODO: O(n^2) too bad should be O(n)
+    for ph_g_idx, ph_g in enumerate(ref_ph_groups):
+        end += len(ph_g)
+        for map_pos in mappings:
+            if map_pos is not None:
+                if start >= map_pos.pos[0] and end <= map_pos.pos[1]:
+                    if ref_tajweed_rules[ph_g_idx] is None:
+                        ref_tajweed_rules[ph_g_idx] = map_pos.tajweed_rules
+                    else:
+                        ref_tajweed_rules[ph_g_idx] += map_pos.tajweed_rules
+        start = end
+    return ref_tajweed_rules
+
+
+def explain_error(
+    uthmani_text, ref_ph_text, predicted_ph_text, mappings: list[MappingPos | None]
+) -> list[ReciterError]:
+    """ """
+    ref_ph_groups = chunck_phonemes(ref_ph_text)
+    pred_ph_groups = chunck_phonemes(predicted_ph_text)
+
+    ref_ph_to_uthmani = extract_ref_phonetic_to_uthmani(mappings)
+    ref_ph_groups_tajweed_rules = get_ref_phonetic_groups_tajweed_rules(
+        ref_ph_groups=ref_ph_groups,
+        mappings=mappings,
+        ref_ph_to_uthmani=ref_ph_to_uthmani,
+    )
+
+    # Aligning Phonemes groups using first chat of  every one
+    alignmets = align_phonemes_groups(ref_ph_groups, pred_ph_groups)
+    errors = []
+    pred_ph_start = 0
+    ref_ph_start = 0
+    pred_ph_end = 0
+    ref_ph_end = 0
+
+    for align in alignmets:
+        ref_ph = ref_ph_groups[align.ref_idx]
+        pred_ph = pred_ph_groups[align.pred_idx]
+        pred_ph_end = pred_ph_start + len(pred_ph)
+
+        if align.op_type != "insert":
+            ref_ph_end = ref_ph_start + len(ref_ph)
+            uthmani_pos = (
+                ref_ph_to_uthmani[ref_ph_start],
+                ref_ph_to_uthmani[ref_ph_end - 1] + 1,
+            )
+            ph_pos = (ref_ph_start, ref_ph_end)
+        else:
+            uthmani_pos = (
+                ref_ph_to_uthmani[ref_ph_start],
+                ref_ph_to_uthmani[ref_ph_start],
+            )
+            ph_pos = (ref_ph_start, ref_ph_start)
+
+        # TODO: for insert and replace try to make explanation for special phoneme like madd, ikhfaa, iqlab, ..
+        if align.op_type == "insert":
+            errors.append(
+                ReciterError(
+                    uthmani_pos=uthmani_pos,
+                    ph_pos=ph_pos,
+                    error_type="normal",  # TODO: try to estimate what is the Tajweed rule associated with this error type
+                    speech_error_type="insert",
+                    expected_ph="",
+                    preditected_ph=pred_ph,
+                )
+            )
+
+        elif align.op_type == "replace":
+            pred_rules = []
+            if ref_ph_groups_tajweed_rules[align.ref_idx] is not None:
+                for taj_rule in ref_ph_groups_tajweed_rules[align.ref_idx]:
+                    pred_taj_rule = taj_rule.get_relvant_rule(pred_ph)
+                    if pred_taj_rule is not None:
+                        # TODO: What to do with `match`
+                        if pred_taj_rule.correctness_type == "count":
+                            ref_len = taj_rule.golden_len
+                            pred_len = pred_taj_rule.count(pred_ph, pred_ph)
+                        else:
+                            ref_len = None
+                            pred_len = None
+                        errors.append(
+                            ReciterError(
+                                uthmani_pos=uthmani_pos,
+                                ph_pos=ph_pos,
+                                error_type="tajweed",
+                                speech_error_type="replace",
+                                expected_ph=ref_ph,
+                                preditected_ph=pred_ph,
+                                expected_len=ref_len,
+                                predicted_len=pred_len,
+                                tajweed_rules=[taj_rule],
+                                predicted_tajweed_rules=[pred_taj_rule],
+                            )
+                        )
+                    else:
+                        errors.append(
+                            ReciterError(
+                                uthmani_pos=uthmani_pos,
+                                ph_pos=ph_pos,
+                                error_type="tajweed",
+                                speech_error_type="replace",
+                                expected_ph=ref_ph,
+                                preditected_ph=pred_ph,
+                                tajweed_rules=[taj_rule],
+                            )
+                        )
+
+            # No Tajweed Rule associated
+            if not ref_ph_groups_tajweed_rules[align.ref_idx]:
+                errors.append(
+                    ReciterError(
+                        uthmani_pos=uthmani_pos,
+                        ph_pos=ph_pos,
+                        error_type="normal",
+                        speech_error_type="replace",
+                        expected_ph=ref_ph,
+                        preditected_ph=pred_ph,
+                    )
+                )
+
+        elif align.op_type == "delete":
+            errors.append(
+                ReciterError(
+                    uthmani_pos=uthmani_pos,
+                    ph_pos=ph_pos,
+                    error_type="tajweed"
+                    if mappings[ref_ph_to_uthmani[ref_ph_start]].tajweed_rules
+                    else "normal",
+                    speech_error_type="delete",
+                    expected_ph=ref_ph,
+                    preditected_ph="",
+                )
+            )
+        elif align.op_type == "equal":
+            if ref_ph == pred_ph:
+                ...
+            # We have Tajweed rule
+            elif ref_ph_groups_tajweed_rules[align.ref_idx] is not None:
+                for taj_rule in ref_ph_groups_tajweed_rules[align.ref_idx]:
+                    exp_len = None
+                    pred_len = None
+                    if taj_rule.correctness_type == "count":
+                        pred_len = taj_rule.count(ref_ph, pred_ph)
+                        exp_len = taj_rule.golden_len
+                    # TODO: What to do with `match`
+                    elif taj_rule.correctness_type == "match":
+                        ...
+                    else:
+                        raise ValueError(
+                            f"Invalid mathing type: `{taj_rule.correctness_type}`. Available: `match`, `count`"
+                        )
+                    errors.append(
+                        ReciterError(
+                            uthmani_pos=uthmani_pos,
+                            ph_pos=ph_pos,
+                            error_type="tajweed",
+                            speech_error_type="replace",  # TODO: make it compliant with speech typedelete
+                            expected_ph=ref_ph,
+                            preditected_ph=pred_ph,
+                            expected_len=exp_len,
+                            predicted_len=pred_len,
+                            tajweed_rules=[taj_rule],
+                        )
+                    )
+
+                # Tashkeel (Harakat)
+                # TODO:
+            elif ref_ph_groups[align.ref_idx][-1] in alph.phonetic_groups.harakat:
+                ...
+
+            # TODO: imala, sakt, dammao momala
+            elif ref_ph_groups[align.ref_idx][-1] in alph.phonetic_groups.residuals:
+                ...
+
+            else:
+                raise ValueError("Uncaptured Error Explanation")
+
+        pred_ph_start = pred_ph_end
+        ref_ph_start = ref_ph_end
+
+    return errors
+
+
+if __name__ == "__main__":
+    uthmani_text = "قالوا"
+    ph_text = "قاالۥۥ"
+    predicted_text = "كالۥۥ"
+    predicted_text = "فكالۥۥ"
+    predicted_text = "فكۥۥلۥۥ"
+
+    normal_madd_alif = NormalMaddRule(tag="alif")
+    normal_madd_waw = NormalMaddRule(tag="waw")
+
+    mapping = [
+        MappingPos(pos=(0, 1)),
+        MappingPos(pos=(1, 3), tajweed_rules=[normal_madd_alif]),
+        MappingPos(pos=(3, 4)),
+        MappingPos(pos=(4, 6), tajweed_rules=[normal_madd_waw]),
+        None,
+    ]
+    errors = explain_error(
+        uthmani_text=uthmani_text,
+        ref_ph_text=ph_text,
+        predicted_ph_text=predicted_text,
+        mappings=mapping,
+    )
+    for err in errors:
+        print(err)
---
+++ src/quran_transcript/phonetics/tajweed_rulses.py
@@ -0,0 +1,99 @@
+from abc import abstractmethod, ABC
+from typing import Literal, Optional
+from dataclasses import dataclass, replace
+
+
+from .. import alphabet as alph
+
+
+@dataclass
+class LangName:
+    ar: str
+    en: str
+
+
+@dataclass
+class TajweedRule(ABC):
+    name: LangName
+    golden_len: int
+    correctness_type: Literal["match", "count"]
+    tag: Optional[str] | None = None
+    available_tags: Optional[set] | None = None
+
+    def __post_init__(self):
+        if self.tag is not None and self.available_tags is not None:
+            if self.tag not in self.available_tags:
+                raise ValueError(
+                    f"Invalid tag value: `{self.tag}`. Available ones are: `{self.available_tags}`"
+                )
+
+    def count(self, ref_text, pred_text) -> int:
+        return 0
+
+    def match(self, ref_text, pred_text) -> bool:
+        return True
+
+    @abstractmethod
+    def is_ph_str_in(self, ph_str: str) -> bool:
+        """Whether the phonetic script is assoicated with this Tajweed rule or not"""
+        return True
+
+    @abstractmethod
+    def get_relvant_rule(self, ph_str: str) -> "TajweedRule" | None:
+        """Returs a Tajweed rule that is assocaited with the input ph_str"""
+        return self
+
+
+@dataclass
+class MaddRule(TajweedRule):
+    name: LangName
+    golden_len: int
+    correctness_type: Literal["match", "count"] = "count"
+
+    def __post_init__(self):
+        self.available_tags = {"alif", "waw", "yaa"}
+        super().__post_init__()
+        self._madd_to_tag = {
+            alph.phonetics.alif: "alif",
+            alph.phonetics.waw_madd: "waw",
+            alph.phonetics.yaa_madd: "yaa",
+        }
+
+    def count(self, ref_text, pred_text) -> int:
+        # The case where we have Tashkeel after madd (Error from the model)
+        if pred_text[-1] != pred_text[0]:
+            return pred_text[:-1].count(ref_text[0])
+        else:
+            return pred_text.count(ref_text[0])
+
+    def is_ph_str_in(self, ph_str: str) -> bool:
+        """Whether the phonetic script is assoicated with this Tajweed rule or not"""
+        if ph_str:
+            return ph_str[0] in self._madd_to_tags
+        else:
+            return False
+
+    def get_relvant_rule(self, ph_str: str) -> "TajweedRule" | None:
+        """Returs a Tajweed rule that is assocaited with the input ph_str"""
+        if not ph_str:
+            raise ValueError("Empty String")
+        elif ph_str[0] not in self._madd_to_tag:
+            return None
+        return replace(self, tag=self._madd_to_tag[ph_str[0]])
+
+
+@dataclass
+class NormalMaddRule(MaddRule):
+    name: LangName = LangName(ar="المد الطبيعي", en="Normal Madd")
+    golden_len: int = 2
+
+
+# TODO:
+
+"""
+* Ghonna
+* Madd
+* Qalaqlah
+* Idgham
+* Tashdeed
+"""
